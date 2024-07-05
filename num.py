558a67ee57acdbfa1dbe28c410e9ace3bc66cca4
Lois Ansah <loiscodes@gmail.com>
2024-07-04 21:29:29 -0400

    Add initial project files and .gitignore

---
+++ num.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+#creatd a 2D array
+
+my_array = np.array([[1,2,3], [4,5,6],[7,8,9]])
+
+#print the array
+print(my_array)
+
+#get shape of the array
+print(my_array.shape)
+
+#get the datatype of the array
+print(my_array.dtype)
