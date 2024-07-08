e7c81cbad447a145bea0c6aaa1b5b22f6c97cc73
Lois Ansah <loiscodes@gmail.com>
2024-07-07 20:31:02 -0400

    Finished exerciced 2, exercise 3 in progress

--- exercice2.py
+++ exercice2.py
@@ -1,19 +1,27 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt 
+#import seaborn as sns
+import warnings as wrn
 
 #import, read file then print its content
 print("FOR: GENGER SUMBISSION.CSV\n ")
 fileI = 'gender_submission.csv'
 data_frame = pd.read_csv(fileI)
 print(data_frame.head())
+print()
+#find number of (rows, columns)
+data_frame.shape
+print()
 
 #dropping rows wth missing values
 print("Dropping rows with missing values")
 print(data_frame.dropna())
 
+#data_frame.shape
 
 
-
+'''
 
 
 print("\nFOR TEST.CSV\n")
@@ -24,3 +32,4 @@ print(df2.head)
 print("Dropping rows with missing values")
 print(df2.dropna())
 
+'''
